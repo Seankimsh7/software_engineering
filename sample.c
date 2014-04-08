eaee8d3f343dc17c2b270874fe4cb1d21c304e08
junsik <jjs5919@nate.com>
2014-04-08 22:03:44 +0900

    window.h 헤더추가 및 cls 소스수정

--- sample.c
+++ sample.c
@@ -1,4 +1,4 @@
-#include<conio.h>
+//#include<conio.h>
 #include<stdio.h>
 #include <stdlib.h>
 
@@ -15,7 +15,7 @@ void cre(void);
 void main()
 {
     int user=0;
-
+    system("cls");
 
     while(user!=4)
       {
